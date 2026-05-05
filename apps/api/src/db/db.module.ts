ceddd320de5b940074a0cdb384c0220d5e87d0bf
Artiom <artiom.sin@gmail.com>
2026-05-05 20:45:49 +0300

    chore: initialize Drizzle ORM and database connection

---
+++ apps/api/src/db/db.module.ts
@@ -0,0 +1,14 @@
+import { Module, Global } from '@nestjs/common';
+import { db } from './db';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'DB',
+      useValue: db,
+    },
+  ],
+  exports: ['DB'],
+})
+export class DbModule {}
