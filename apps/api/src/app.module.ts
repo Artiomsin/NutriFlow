cecd71ace7a3d9e634718d8cc2f47c0c228a342a
Artiom <artiom.sin@gmail.com>
2026-05-06 16:31:48 +0300

    chore(core): setup config and database module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from "@nestjs/common";
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfilesModule } from './profiles/profiles.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProfilesModule],
+  imports: [UsersModule,DbModule, AuthModule, ProfilesModule,
+    ConfigModule.forRoot({
+        isGlobal: true,
+      }),],
 })
 export class AppModule {}
